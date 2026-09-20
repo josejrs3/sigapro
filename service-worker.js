f11eaf48245d741e477c12cb3fb88102d3767b5e
josejrs3 <contato.josejrs+2@gmail.com>
2026-09-20 00:27:16 +0000

    gof

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'siga-shell-v1';
+const CACHE_NAME = 'siga-shell-v2';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
